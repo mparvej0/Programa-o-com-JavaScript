8ad6cf8602257ccdb1b80b5e5207a394f5b424ef
MD PARVEJ <mparvejind@gmail.com>
2025-01-03 00:16:28 +0530

    upd

--- Operators and Conditional statements/operator.js
+++ Operators and Conditional statements/operator.js
@@ -11,3 +11,46 @@ a++; //Or ++a
 console.log("a =", a);
 b--; //Or --b
 console.log("b =", b);
+
+//Assignment Operators
+let c = 7;
+let d = 4;
+let e = 9;
+let f = 6;
+let g = 11;
+let h = 8;
+c = 4;
+console.log("c =", c);
+d += 4;
+console.log("d =", d);
+e -= 4;
+console.log("e =", e);
+f *= 4;
+console.log("f =", f);
+g %= 4;
+console.log("g =", g);
+h **= 4;
+console.log("h =", h);
+
+//Comparison Operators
+let i = 13;
+let j = 10;
+console.log("i == j", i == j);
+console.log("i != j", i != j);
+console.log("i === j", i === j);
+console.log("i !== j", i !== j);
+console.log("i > j", i > j);
+console.log("i >= j", i >= j);
+console.log("i < j", i < j);
+console.log("i <= j", i <= j);
+
+//Logical Operators
+let k = 15;
+let l = 12;
+console.log("k && l", k && l);
+console.log("k || l", k || l);
+console.log("!(k<l) =", !(k < l));
+
+
+
+
