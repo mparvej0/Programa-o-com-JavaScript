e65aa7ed4b896ed4c535d14e91c956b2f11ca36b
MD PARVEJ <mparvejind@gmail.com>
2025-01-03 00:31:39 +0530

    upd

---
+++ Operators and Conditional statements/conditional_statement.js
@@ -0,0 +1,30 @@
+//if Statement
+let age = 25;
+if(age > 18); {
+    console.log("You can vote");
+}
+if (age < 18) {
+    console.log("You can't vote");
+}
+
+let mode = "dark";
+let color;
+if(mode == "dark") {
+    color = "black";
+}
+if(mode = "light") {
+    color = "white";
+}
+console.log(color);
+
+//if-else Statement
+let mode1 = "light";
+let color1;
+if(mode1 == "dark") {
+    color1 = "black";
+} else {
+    color1 = "white";
+}
+console.log(color1);
+
+//else-if Statement
