5188e351a0169f2473e9039453fb11ce2ee015d7
MD PARVEJ <mparvejind@gmail.com>
2025-01-02 20:46:11 +0530

    upd

---
+++ Operators and Conditional statements/operator.js
@@ -0,0 +1,13 @@
+//Arithmetic Operators
+let a = 5;
+let b = 2;
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b);
+a++; //Or ++a
+console.log("a =", a);
+b--; //Or --b
+console.log("b =", b);
