dcded58e58c57c3e12d14584731016ec34d52ade
MD PARVEJ <mparvejind@gmail.com>
2025-01-03 15:12:49 +0530

    upd

---
+++ Loops and Strings/string.js
@@ -0,0 +1,11 @@
+//Create String
+let str = "Hello World!";
+console.log(str)
+
+//String Length
+let str1 = "Adiba Maher";
+console.log(str1, str1.length)
+
+//String indices
+let str2 = "Soas University of London"
+console.log(str2, str2[0], str[1], str[2])
