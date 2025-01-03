6bf588ecd9472458943fd08d90cd9c75e5698c55
MD PARVEJ <mparvejind@gmail.com>
2025-01-03 17:59:08 +0530

    upd

--- Loops and Strings/prac.js
+++ Loops and Strings/prac.js
@@ -12,4 +12,6 @@ userNum = prompt("You Entered wrong number Guess agian : ");
 console.log("Congratulations, You Enteredthe right number");
 
 //Qs1. Prompt the user to enter their full name. Generate a username for them based on the input. Start username with @, followed by their full name and ending with the fullname length. eg: user name =“shradhakhapra”, username should be “@shradhakhapra13"
-
+let fullName = prompt("Enter Yor fullName without spaces");
+let username = "@" + fullName + fullName.length;
+console.log(fullName);
--- Loops and Strings/string.js
+++ Loops and Strings/string.js
@@ -11,3 +11,16 @@ let str2 = "Soas University of London"
 console.log(str2, str2[0], str[1], str[2])
 
 //Methods
+let str3 = "Soas Universtity of London";
+let newStr = str3.toUpperCase();
+let newStr1 = str3.toLowerCase();
+console.log(newStr);
+console.log(newStr1);
+console.log(str3.trim());
+console.log(str3.slice(1, 7));
+let str4 = "Data Science & Application";
+let str5 = " from IITM";
+console.log(str4.concat(str5));
+let str6 = "Abiba";
+console.log(str6.replace("b", "d")); ///replaceAll
+console.log(str4.charAt(7))
