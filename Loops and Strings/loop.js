a65a25a61e094f1a1cbab82046dddaf543fb7365
MD PARVEJ <mparvejind@gmail.com>
2025-01-03 13:26:37 +0530

    upd

---
+++ Loops and Strings/loop.js
@@ -0,0 +1,31 @@
+//for Loop
+for(let count = 1; count <= 5; count++) {
+    console.log("Hello World!");
+}
+
+//while Loop
+let i = 1;
+while(i<=5) {
+    console.log("i =", i);
+    i++;
+}
+let j = 1;
+while (j <= 3) {
+    console.log("Raju")
+    j++;
+}
+
+//do-while Loop
+let k = 17;
+do {
+    console.log("Mr. Raju");
+    k++;
+} while (k <= 11);
+
+let x = 1;
+do {
+    console.log("x =", x);
+    x++;
+} while (x <= 5);
+
+//for-of Loop
