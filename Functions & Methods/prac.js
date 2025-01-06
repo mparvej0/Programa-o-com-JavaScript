8abc5f816e6f142fb708085d993ed498b34cbf70
MD PARVEJ <mparvejind@gmail.com>
2025-01-06 14:17:55 +0530

    upd

---
+++ Functions & Methods/prac.js
@@ -0,0 +1,12 @@
+// Qs1. Create a function using the “function” keyword that takes a String as an argument &
+// returns the number of vowels in the string.
+
+// Qs2. Create an arrow function to perform the same task.
+
+// Qs3. For a given array of numbers, print the square of each value using the forEach loop.
+
+// Qs4. We are given array of marks of students. Filter our of the marks of students that scored 90+.
+
+// Qs5. Take a number n as input from user. Create an array   from 1 to n.
+// Use the reduce method to calculate sum of all numbers in the array.
+// Use the reduce method to calculate product of all numbers in the array.
