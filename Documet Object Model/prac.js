f3db72ea62eadc17315281c8a0cc13034d1a7432
MD PARVEJ <mparvejind@gmail.com>
2025-01-06 17:34:12 +0530

    upd

--- Documet Object Model/prac.js
+++
@@ -1,4 +0,0 @@
-// Qs1. Create a new button element. Give it a text “click me”, background color of red & text color of white.
-// Insert the button as the first element inside the body tag.
-
-// Qs2. Create a <p> tag in html, give it a class & some styling. Now create a new class in CSS and try to append this class to the <p> element. Solve this problem using classList. Did you notice, how you overwrite the class name when you add a new one? Solve this problem using classList.
