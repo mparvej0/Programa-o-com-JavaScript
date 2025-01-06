9b95727402288e862056d3cb374b0c7d4d70b851
MD PARVEJ <mparvejind@gmail.com>
2025-01-06 13:39:18 +0530

    upd

--- Arrays/array.js
+++ Arrays/array.js
@@ -11,4 +11,21 @@ let heros = ["ironman", "hulk", "thor", "batman"];
 for(let i = 0; i < heros.length; i++) {
     console.log(heros[i]);
 }
+
 //Array Methods
+let foodItems = ["Potato", "Apple", "Litchi", "Tomato"]
+foodItems.push("Chips", "Burger", "Paneer");
+console.log(foodItems);
+foodItems.pop();
+console.log(foodItems);
+console.log(foodItems.toString());
+let veggies = ["Brinjal", "Lady finger", "Onion"]
+let veg = foodItems.concat(veggies);
+console.log(veg);
+foodItems.unshift("Cucumber");
+console.log(foodItems)
+foodItems.shift("Litchi")
+console.log(foodItems);
+console.log(foodItems.slice(1, 4));
+foodItems.splice("Watermelon", "Mango", "Gauva");
+console.log(foodItems);
