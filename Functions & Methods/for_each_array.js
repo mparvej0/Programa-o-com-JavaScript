b86eda323b9e13ca31a94d0fb47747064655370f
MD PARVEJ <mparvejind@gmail.com>
2025-01-06 15:13:24 +0530

    upd

---
+++ Functions & Methods/for_each_array.js
@@ -0,0 +1,4 @@
+let arr = [1, "Pune", 2, "Mumbai", 3, "Delhi", 4, "Kolkata", 5];
+arr.forEach((val) => {
+    console.log(val);
+});
--- Functions & Methods/prac.js
+++ Functions & Methods/prac.js
@@ -1,7 +1,25 @@
 // Qs1. Create a function using the “function” keyword that takes a String as an argument &
 // returns the number of vowels in the string.
+function countVowels(str) {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" ||char === "o" || char === "u") {
+            count++;
+        }
+    }
+    console.log(count);
+}
 
 // Qs2. Create an arrow function to perform the same task.
+const countVow = (str) => {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" ||char === "o" || char === "u") {
+            count++;
+        }
+    }
+    console.log(count);
+}
 
 // Qs3. For a given array of numbers, print the square of each value using the forEach loop.
 
