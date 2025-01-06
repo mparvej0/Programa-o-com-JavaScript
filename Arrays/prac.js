ab42099b76f081107a5a2a6e6342d39c88ec1fd3
MD PARVEJ <mparvejind@gmail.com>
2025-01-06 13:44:24 +0530

    upd

--- Arrays/prac.js
+++ Arrays/prac.js
@@ -24,3 +24,10 @@ for (let val of items) {
 // a. Remove the first company from the array
 // b. Remove Uber & Add Ola in its place
 // c. Add Amazon at the end
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+companies.shift();
+console.log(companies)
+companies.splice(2, 1, "Ola");
+console.log(companies);
+companies.push("Amazon");
+console.log(companies);
