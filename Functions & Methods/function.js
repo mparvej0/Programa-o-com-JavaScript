59c8586568f8490ad526bad6fe8ec5072e14c5db
MD PARVEJ <mparvejind@gmail.com>
2025-01-06 14:49:14 +0530

    upd

--- Functions & Methods/function.js
+++ Functions & Methods/function.js
@@ -1,1 +1,13 @@
-functio
+function myFunction() {
+    console.log("Welcome to IITM!");
+    console.log("We are learning JS : ");
+};
+myFunction();
+
+function sum(x, y) {
+    s = x + y;
+    console.log("Before return");
+    return s;
+}
+let val = sum(3, 4);
+console.log(val);
